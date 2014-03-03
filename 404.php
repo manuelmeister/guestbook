c63892673e71880b8b9b01a7d256bc5ee8fdcf0a
Manuel Meister <news.manuelsworld@gmail.com>
2014-03-03 15:31:52 +0100

    Added 404 Error page

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manuelmeister
+ * Date: 03.03.14
+ * Time: 15:11
+ */
+include 'functions.php';
+include 'header.php';
+header("HTTP/1.0 404 Not Found");
+echo '<div id="content" style="clear: both">
+        <div class="entry error">404 Seite nicht gefunden!</div>
+      </div>';
+include 'footer.php';
